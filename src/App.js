1bdf35b185f0667cf288961861ca5b4efa9c57b9
Micaela Lechmann <micaela.lechmann@gmail.com>
2020-03-21 21:31:56 -0300

    add person component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
+import React, { useState, Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Person from './Person/Person'
 
-function App() {
+ class App extends Component{
+  state = {
+    persons: [
+      {name: 'Mica', age: 19 },
+      {name: 'Rodrigo', age: 23}
+    ]
+  }
+
+render() { 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person name={this.state.persons[0].name} age="19">Lalalala</Person>
+      <Person name="Rodrigo" age="23"></Person>
+      <Person name="Martina" age="21"></Person>
     </div>
   );
-}
+}}
 
 export default App;
